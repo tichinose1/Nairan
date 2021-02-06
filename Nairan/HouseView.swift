22cae7bda2859eccef893f279b8194933c2ddff0
tichinose1 <tichinose1dev@gmail.com>
2021-02-06 23:22:17 +0900

    Add View about houses

---
+++ Nairan/HouseView.swift
@@ -0,0 +1,20 @@
+//
+//  HouseView.swift
+//  Nairan
+//
+//  Created by tichinose1 on 2021/02/06.
+//
+
+import SwiftUI
+
+struct HouseView: View {
+    var body: some View {
+        Text("HouseView")
+    }
+}
+
+struct HouseView_Previews: PreviewProvider {
+    static var previews: some View {
+        HouseView()
+    }
+}
