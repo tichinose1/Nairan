dcf5c67b23ec0e7c72c744a13a968f66a303661e
tichinose1 <tichinose1dev@gmail.com>
2021-02-15 14:31:51 +0900

    Close PHPicker successfully!

--- Nairan/ImagePickerView.swift
+++ Nairan/ImagePickerView.swift
@@ -10,11 +10,32 @@ import PhotosUI
 
 struct ImagePickerView: UIViewControllerRepresentable {
 
+    @Environment(\.presentationMode) private var presentationMode
+
     func makeUIViewController(context: Context) -> some UIViewController {
         let configuration = PHPickerConfiguration()
-        return PHPickerViewController(configuration: configuration)
+        let controller = PHPickerViewController(configuration: configuration)
+        controller.delegate = context.coordinator
+        return controller
     }
 
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+
+        var parent: ImagePickerView
+
+        init(_ parent: ImagePickerView) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
