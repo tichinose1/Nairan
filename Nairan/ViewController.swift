714c3e9fa5c7442448e75addeb0edcc625d39859
tichinose1 <tichinose1dev@gmail.com>
2021-02-19 21:00:41 +0900

    Recreate a project(not SwiftUI)

---
+++ Nairan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nairan
+//
+//  Created by tichinose1 on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
