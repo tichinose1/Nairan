c21a59c545f8ecc3017e4421e8853bd531284cb7
tichinose1 <tichinose1dev@gmail.com>
2021-03-06 23:57:53 +0900

    Update sample data

--- Nairan/Defect.swift
+++ Nairan/Defect.swift
@@ -15,7 +15,7 @@ struct Defect {
 extension Defect {
     static var items: [Defect] = [
         Defect(title: "汚れている", image: URL(string: "https://www.jyutakusindan.com/wp-content/uploads/sites/9/2016/11/2b530e80c7d0de90885e285c5d798063.png")),
-        Defect(title: "傾いている"),
+        Defect(title: "傾いている", image: URL(string: "https://stat.ameba.jp/user_images/20121223/14/mikami2345/4d/f8/j/o0420031512342471305.jpg?caw=800")),
         Defect(title: "穴があいている")
     ]
 }
