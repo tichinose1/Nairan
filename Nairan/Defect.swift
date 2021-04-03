f8bd0ac9d970640e0e273485a9104bcb79e22582
tichinose1 <tichinose1dev@gmail.com>
2021-04-03 23:11:18 +0900

    Update sample data

--- Nairan/Defect.swift
+++ Nairan/Defect.swift
@@ -17,17 +17,17 @@ struct Defect {
 
 extension Defect {
     static var items: [Defect] = [
-        Defect(title: "壁が汚れている",
+        Defect(title: "リビングの壁①",
                imageURL: URL(string: "https://www.jyutakusindan.com/wp-content/uploads/sites/9/2016/11/2b530e80c7d0de90885e285c5d798063.png")!,
-               detail: "detail1"
+               detail: "壁が汚れている"
         ),
-        Defect(title: "床が傾いている",
+        Defect(title: "リビングの床①",
                imageURL: URL(string: "https://stat.ameba.jp/user_images/20121223/14/mikami2345/4d/f8/j/o0420031512342471305.jpg?caw=800")!,
-               detail: "detail2"
+               detail: "床が傾いている"
         ),
-        Defect(title: "穴があいている",
+        Defect(title: "寝室の壁①",
                imageURL: URL(string: "https://homedoctor.co.jp/servise/img/nairan3.jpg")!,
-               detail: "detail3"
+               detail: "穴があいている"
         )
     ]
 }
